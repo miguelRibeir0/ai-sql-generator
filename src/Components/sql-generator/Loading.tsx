987abad78edb4e23b6e09c0f19808f2d54e0c703
Miguel <miguelsribeiro7@gmail.com>
2024-10-07 01:19:37 +0100

    finished the sql gen

---
+++ src/Components/sql-generator/Loading.tsx
@@ -0,0 +1,12 @@
+import { Skeleton } from "@/Components/ui/skeleton";
+import { Fade } from "react-awesome-reveal";
+
+export default function Loading() {
+	return (
+		<Fade className="absolute top-[58%] mt-4">
+			<div className="overflow-hidden rounded-lg p-1 border bg-background w-60 h-12 focus-within:ring-1 focus-within:ring-ring flex items-center justify-center">
+				<Skeleton className="h-full w-full" />
+			</div>
+		</Fade>
+	);
+}
--- src/Components/sql-generator/SqlGenerator.tsx
+++ src/Components/sql-generator/SqlGenerator.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Output from "./Output";
+import Loading from "./Loading";
 import { groqRequest } from "./groqRequest";
 
 import { CornerDownLeft } from "lucide-react";
@@ -9,26 +10,30 @@ import { Label } from "@/Components/ui/label";
 import { Textarea } from "@/Components/ui/textarea";
 
 export default function SqlGenerator() {
-	const [message, setMessage] = useState("");
+	const [prompt, setPrompt] = useState("");
 	const [state, setState] = useState(false);
 	const [sql, setSql] = useState("");
 	const [isLoading, setIsLoading] = useState(false);
 
-	const stringCleanUp = (str: string) => {
-		return str.replace(/```|;/g, "");
-	};
+	// const stringCleanUp = (str: string) => {
+	// 	return str.replace(/```|;/g, "");
+	// };
 
-	const prompt =
-		"Give me a SQL query that returns the entirity of the Kitchen Stock table.";
 	const model = "llama3-8b-8192";
 
 	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault();
+
+		if (prompt.trim().length == 0) {
+			return alert("Please insert a valid prompt");
+		}
+
 		setIsLoading(true);
+		setState(true);
 
 		try {
 			const response = await groqRequest({ prompt, model });
-			setSql(stringCleanUp(response.completion));
+			setSql(response.completion);
 		} catch (error) {
 			console.error("Error fetching SQL:", error);
 		} finally {
@@ -43,18 +48,17 @@ export default function SqlGenerator() {
 					className="relative overflow-hidden rounded-lg border bg-background min-w-72 lg:min-w-96 "
 					onSubmit={(e) => {
 						e.preventDefault();
-						setState(!state);
 						handleSubmit(e);
 					}}
 				>
-					<Label htmlFor="message" className="sr-only">
-						Message
+					<Label htmlFor="prompt" className="sr-only">
+						prompt
 					</Label>
 					<Textarea
-						id="message"
-						placeholder="Type your message here..."
-						value={message}
-						onChange={(e) => setMessage(e.target.value)}
+						id="prompt"
+						placeholder="Type your prompt here..."
+						value={prompt}
+						onChange={(e) => setPrompt(e.target.value)}
 						className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
 					/>
 					<div className="flex items-center p-3 mt-3">
@@ -68,7 +72,11 @@ export default function SqlGenerator() {
 						</Button>
 					</div>
 				</form>
-				{state ? <Output sql={sql} /> : null}
+				{state && isLoading ? (
+					<Loading />
+				) : state && !isLoading ? (
+					<Output sql={sql} />
+				) : null}
 			</div>
 		</>
 	);
